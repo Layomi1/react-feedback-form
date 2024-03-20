cd81aab2eabe7e536e5e6768e3101419f2af6514
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2024-03-20 10:21:36 +0100

    creating a form with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [score, setScore] = useState("10");
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Number(score) <= 5 && comment.length <= 10) {
+      alert("Please, provide a comment explaining why the experience poor.");
+      console.log("form submitted");
+      setComment("");
+    }
+    console.log("submitted");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Feedback form</h2>
+          <div className="field">
+            <label htmlFor="score">Score:{score} ⭐</label>
+            <input
+              type="range"
+              min="0"
+              max="10"
+              value={score}
+              onChange={(e) => setScore(e.target.value)}
+            />
+          </div>
+          <div className="comment-area">
+            <label htmlFor="comment">Comment: </label>
+            <textarea
+              rows="10"
+              columns="30"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+          </div>
+          <button type="Submit">Submit</button>
+        </fieldset>
+      </form>
     </div>
   );
 }
